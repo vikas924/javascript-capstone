a1ecd6810bde0310ddf9ec12a7ec17961864333c
vikas924 <obroy902@gmail.com>
2023-06-27 18:25:02 +0530

    Add modal and add modal eventlistener function

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -1,4 +1,4 @@
-import fetchShows from './fetchList.js';
+import { fetchShows, fetchdata } from './fetchList.js';
 import { baseUrl } from './config.js';
 
 const displayShows = async () => {
@@ -27,6 +27,42 @@ const displayShows = async () => {
                      </div>`;
   });
   showList.innerHTML = html;
+
+  const commentsbutton = document.querySelectorAll('.comments');
+  commentsbutton.forEach((comments) => comments.addEventListener('click', async (event) => {
+    const modal = document.getElementById('Modal');
+    const Id = event.target.getAttribute('data-showid');
+    const showdata = await fetchdata(`${baseUrl}/${Id}`);
+    const seasons = await fetchdata(`${baseUrl}/${Id}/seasons`);
+    let genrehtml = '';
+    showdata.genres.forEach((genre, index) => {
+      if ((index + 1) !== showdata.genres.length) {
+        genrehtml += `${genre},`;
+      } else if ((index + 1) === showdata.genres.length) {
+        genrehtml += `${genre}.`;
+      }
+    });
+    modal.classList.add('show');
+    modal.setAttribute('aria-hidden', 'false');
+    modal.style.display = 'block';
+    document.body.classList.add('modal-open');
+
+    document.getElementById('showImage').setAttribute('src', showdata.image.medium);
+    document.getElementById('showTitle').innerHTML = showdata.name;
+    document.getElementById('showLang').innerHTML = `Language: ${showdata.language}`;
+    document.getElementById('showType').innerHTML = `Type: ${showdata.type}`;
+    document.getElementById('showPremiered').innerHTML = `Genre: ${genrehtml}`;
+    document.getElementById('showEnd').innerHTML = `Seasons: ${seasons.length}`;
+  }));
+
+  const modalclose = document.querySelector('#closeIcon');
+  modalclose.addEventListener('click', () => {
+    const modal = document.getElementById('Modal');
+    modal.classList.remove('show');
+    modal.setAttribute('aria-hidden', 'false');
+    modal.style.display = 'none';
+    document.body.classList.remove('modal-open');
+  });
 };
 
 export default displayShows;
